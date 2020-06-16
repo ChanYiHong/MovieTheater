18849e405b08c26245935e00deb135ab0541119e
Euna <98dmsdk@naver.com>
2020-06-17 01:04:14 +0900

    click event listener and change selected target's class

--- reservation.js
+++ reservation.js
@@ -2,8 +2,20 @@ window.onload = function(){
     var movie_lists = document.querySelectorAll('#movie_contents_list li');
     
     for(var i=0; i<movie_lists.length; i++){
-        movie_lists[i].addEventListener('click', function(){
-            alert("click id", movie_lists.value);
+        movie_lists[i].addEventListener('click', function(event){
+            var className = this.getAttribute('class');
+            var value;
+            switch(className){
+                case 'movie_rate_all': value ="movie_all_selected"; break;
+                case 'movie_all_selected': value ="movie_rate_all"; break;
+                case 'movie_rate_12': value ="movie_12_selected"; break;
+                case 'movie_12_selected': value ="movie_rate_12"; break;
+                case 'movie_rate_15': value ="movie_15_selected"; break;
+                case 'movie_15_selected': value ="movie_rate_15"; break;
+                case 'movie_rate_19': value ="movie_19_selected"; break;
+                case 'movie_19_selected': value ="movie_rate_19"; break;
+            }
+            this.setAttribute('class', value);
         });
     }
 }
