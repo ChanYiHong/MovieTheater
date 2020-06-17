48145c131666808deecb7b2694904216592ecfc0
Euna <98dmsdk@naver.com>
2020-06-17 14:08:17 +0900

    MovieSelect module

--- reservation.js
+++ reservation.js
@@ -1,29 +1,35 @@
 var selected_movie_id; //선택된 movie의 id
 
 window.onload = function(){
-    /* 영화 Title 선택 List */
+   
+    // movie select list event listener
     var movie_lists = document.querySelectorAll('#movie_contents_list li');
     for(var i=0; i<movie_lists.length; i++){
         movie_lists[i].addEventListener('click', function(event){
-            var clicked_movie_id = this.getAttribute('id');
-            //선택된 영화가 아예 없음
-            if(selected_movie_id===undefined){
-                movieCheckedState(this, clicked_movie_id);
-            }else{
-                //선택됐던 영화를 unchecked로
-                movieUncheckedState();
-                if(selected_movie_id === clicked_movie_id){
-                    //선택한 영화 == 클릭한 영화 > 아무것도 선택 X
-                    selected_movie_id = undefined;
-                }else{        
-                    //새로 클릭한 영화가 checked
-                    movieCheckedState(this, clicked_movie_id);
-                }      
-            }
-            console.log("Selected movie:",selected_movie_id);
+            movieSelect(this);
         });
     }
-     /* /영화 Title 선택 List */
+    
+}
+
+ /* 영화 Title 선택 List */
+function movieSelect(clicked_movie){
+    var clicked_movie_id = clicked_movie.getAttribute('id');
+    //선택된 영화가 아예 없음
+    if(selected_movie_id===undefined){
+        movieCheckedState(clicked_movie, clicked_movie_id);
+    }else{
+        //선택됐던 영화를 unchecked로
+        movieUncheckedState();
+        if(selected_movie_id === clicked_movie_id){
+            //선택한 영화 == 클릭한 영화 > 아무것도 선택 X
+            selected_movie_id = undefined;
+        }else{        
+            //새로 클릭한 영화가 checked
+            movieCheckedState(clicked_movie, clicked_movie_id);
+        }      
+    }
+    console.log("Selected movie:",selected_movie_id);
 }
 
 function movieCheckedState(clicked_movie, clicked_movie_id){
@@ -50,3 +56,5 @@ function movieUncheckedState(){
     }
     selected_movie.className=value;
 }
+
+ /* /영화 Title 선택 List */
