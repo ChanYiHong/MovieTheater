c65a41d1f02a02aefbd147d88c47eff0ac5956b9
ChanYiHong <chanyinara@naver.com>
2020-09-10 23:59:48 +0900

    TimeTable service & dtos

---
+++ src/main/java/ChanuE/MovieTheater/dto/TimeTable/TimeTableSaveRequestDto.java
@@ -0,0 +1,9 @@
+package ChanuE.MovieTheater.dto.TimeTable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TimeTableSaveRequestDto {
+}
--- src/main/java/ChanuE/MovieTheater/service/TimeTableService.java
+++ src/main/java/ChanuE/MovieTheater/service/TimeTableService.java
@@ -1,4 +1,32 @@
 package ChanuE.MovieTheater.service;
 
+import ChanuE.MovieTheater.domain.Seat;
+import ChanuE.MovieTheater.domain.TimeTable;
+import ChanuE.MovieTheater.dto.TimeTable.TimeTableResponseDto;
+import ChanuE.MovieTheater.dto.TimeTable.TimeTableSaveRequestDto;
+import ChanuE.MovieTheater.dto.seat.SeatResponseDto;
+import ChanuE.MovieTheater.dto.seat.SeatSaveRequestDto;
+import ChanuE.MovieTheater.repository.TimeTableRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
 public class TimeTableService {
+
+    private final TimeTableRepository timeTableRepository;
+
+    @Transactional
+    public void saveTimeTable(TimeTableSaveRequestDto requestDto){
+        TimeTable timeTable = requestDto.toEntity();
+        timeTableRepository.save(timeTable);
+    }
+
+    public List<SeatResponseDto> findAllSeat(){
+        return SeatResponseDto.seatToSeatResponseDto(seatRepository.findAll());
+    }
 }
