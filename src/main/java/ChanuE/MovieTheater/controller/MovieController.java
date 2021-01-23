4fba7e1772de9fc5e40db9695ee073709d5431a0
ChanYiHong <chanyinara@naver.com>
2021-01-23 23:58:00 +0900

    Refactoring..

--- src/main/java/ChanuE/MovieTheater/controller/MovieController.java
+++ src/main/java/ChanuE/MovieTheater/controller/MovieController.java
@@ -10,38 +10,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/movie/")
 @RequiredArgsConstructor
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping("/movie/create")
+    @GetMapping("/list")
+    public String movieList(Model model){
+        PageResponseDTO<Movie, MovieResponseDto> result = movieService.findAll();
+        model.addAttribute("result", result);
+        return "/movies/movie_list";
+    }
+
+    @GetMapping("/create")
     public String createMovie(Model model){
         model.addAttribute("movie", new MovieSaveRequestDto());
         return "/movies/movie_create";
     }
 
-    @GetMapping("/api/movie/create")
-    public String createMovieForm(){
-        return "/movies/movie_api_create";
-    }
-
-    @PostMapping("/movie/create")
+    @PostMapping("/create")
     public String saveMovie(MovieSaveRequestDto requestDto){
 
         System.out.println("이름 : " + requestDto.getName());
         movieService.saveMovie(requestDto);
         return "redirect:/";
-    }
-
-    @GetMapping("/movies")
-    public String movieList(Model model){
-        PageResponseDTO<Movie, MovieResponseDto> result = movieService.findAll();
-        model.addAttribute("result", result);
-        return "/movies/movie_list";
     }
 }
